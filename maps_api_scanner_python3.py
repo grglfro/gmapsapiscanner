f0a4e0f5db00d51f66cf5d03a760012b4c5add73
greg alfaro <greg.alfaro@pm.me>
2021-06-16 12:27:31 -0500

    remove specific URL in ref header

--- maps_api_scanner_python3.py
+++ maps_api_scanner_python3.py
@@ -7,7 +7,7 @@ import os
 def scan_gmaps(apikey):
 	vulnerable_apis = []
 	url = "https://www.googleapis.com/customsearch/v1?cx=017576662512468239146:omuauf_lfve&q=lectures&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.text.find("errors") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Custom Search API! Here is the PoC link which can be used directly via browser:")
@@ -18,7 +18,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ str(response.json()["error"]["errors"][0]["message"]))
 
 	url = "https://maps.googleapis.com/maps/api/staticmap?center=45%2C10&zoom=7&size=400x400&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.status_code == 200:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Staticmap API! Here is the PoC link which can be used directly via browser:")
@@ -29,7 +29,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ str(response.content))
 
 	url = "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=40.720032,-73.988354&fov=90&heading=235&pitch=10&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)	
 	if response.status_code == 200:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Streetview API! Here is the PoC link which can be used directly via browser:")
@@ -40,7 +40,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ str(response.content))
 
 	url = "https://www.google.com/maps/embed/v1/place?q=Seattle&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.status_code == 200:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Embed (Basic) API! Here is the PoC HTML code which can be used directly via browser:")
@@ -51,7 +51,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ str(response.content))
 
 	url = "https://www.google.com/maps/embed/v1/search?q=record+stores+in+Seattle&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.status_code == 200:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Embed (Advanced) API! Here is the PoC HTML code which can be used directly via browser:")
@@ -65,7 +65,7 @@ def scan_gmaps(apikey):
 			print("Reason: "+ str(response.content).split("\"")[77])
 
 	url = "https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood4&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.text.find("error_message") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Directions API! Here is the PoC link which can be used directly via browser:")
@@ -77,7 +77,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ response.json()["error_message"])
 
 	url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=40,30&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.text.find("error_message") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Geocode API! Here is the PoC link which can be used directly via browser:")
@@ -88,7 +88,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ response.json()["error_message"])
 
 	url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.6655101,-73.89188969999998&destinations=40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626%7C40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.text.find("error_message") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Distance Matrix API! Here is the PoC link which can be used directly via browser:")
@@ -100,7 +100,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ response.json()["error_message"])
 
 	url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.text.find("error_message") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Find Place From Text API! Here is the PoC link which can be used directly via browser:")
@@ -111,7 +111,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ response.json()["error_message"])
 
 	url = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=Bingh&types=%28cities%29&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.text.find("error_message") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Autocomplete API! Here is the PoC link which can be used directly via browser:")
@@ -123,7 +123,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ response.json()["error_message"])
 
 	url = "https://maps.googleapis.com/maps/api/elevation/json?locations=39.7391536,-104.9847034&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.text.find("error_message") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Elevation API! Here is the PoC link which can be used directly via browser:")
@@ -134,7 +134,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ response.json()["error_message"])
 
 	url = "https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp=1331161200&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.text.find("errorMessage") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Timezone API! Here is the PoC link which can be used directly via browser:")
@@ -145,7 +145,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ response.json()["errorMessage"])
 
 	url = "https://roads.googleapis.com/v1/nearestRoads?points=60.170880,24.942795|60.170879,24.942796|60.170877,24.942796&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.text.find("error") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Nearest Roads API! Here is the PoC link which can be used directly via browser:")
@@ -156,7 +156,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ response.json()["error"]["message"])
 
 	url = "https://www.googleapis.com/geolocation/v1/geolocate?key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	postdata = {'considerIp': 'true'}
 	response = requests.get(url, headers=headers, data=postdata, verify=False)
 	if response.text.find("error") < 0:
@@ -173,7 +173,7 @@ def scan_gmaps(apikey):
 
 
 	url = "https://roads.googleapis.com/v1/snapToRoads?path=-35.27801,149.12958|-35.28032,149.12907&interpolate=true&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.text.find("error") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Route to Traveled API! Here is the PoC link which can be used directly via browser:")
@@ -184,7 +184,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ response.json()["error"]["message"])
 
 	url = "https://roads.googleapis.com/v1/speedLimits?path=38.75807927603043,-9.03741754643809&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.text.find("error") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Speed Limit-Roads API! Here is the PoC link which can be used directly via browser:")
@@ -195,7 +195,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ response.json()["error"]["message"])
 
 	url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name,rating,formatted_phone_number&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)	
 	if response.text.find("error_message") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Place Details API! Here is the PoC link which can be used directly via browser:")
@@ -206,7 +206,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ response.json()["error_message"])
 
 	url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=100&types=food&name=harbour&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.text.find("error_message") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Nearby Search-Places API! Here is the PoC link which can be used directly via browser:")
@@ -217,7 +217,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ response.json()["error_message"])
 
 	url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, headers=headers, verify=False)
 	if response.text.find("error_message") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Text Search-Places API! Here is the PoC link which can be used directly via browser:")
@@ -228,7 +228,7 @@ def scan_gmaps(apikey):
 		print("Reason: "+ response.json()["error_message"])
 
 	url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key="+apikey
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.get(url, verify=False, headers=headers, allow_redirects=False)
 	if response.status_code == 302:
 		print("API key is \033[1;31;40m vulnerable \033[0m for Places Photo API! Here is the PoC link which can be used directly via browser:")
@@ -240,7 +240,7 @@ def scan_gmaps(apikey):
 
 	url = "https://playablelocations.googleapis.com/v3:samplePlayableLocations?key="+apikey
 	postdata = {'area_filter':{'s2_cell_id':7715420662885515264},'criteria':[{'gameObjectType':1,'filter':{'maxLocationCount':4,'includedTypes':['food_and_drink']},'fields_to_return': {'paths': ['name']}},{'gameObjectType':2,'filter':{'maxLocationCount':4},'fields_to_return': {'paths': ['types', 'snapped_point']}}]}
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
+	headers = {'Referer': 'https://www.example.com/exampletext'}
 	response = requests.post(url, data=postdata, headers=headers, verify=False)
 	if response.text.find("error") < 0:
 		print("API key is \033[1;31;40m vulnerable \033[0mfor Playable Locations API! Here is the PoC curl command which can be used from terminal:")
@@ -252,8 +252,8 @@ def scan_gmaps(apikey):
 
 	url = "https://fcm.googleapis.com/fcm/send"
 	postdata = "{'registration_ids':['ABC']}"
-	headers = {'Referer': 'https://www.synack.com/exampletext'}
-	response = requests.post(url, data=postdata, verify=False, headers={'Referer': 'https://www.synack.com/exampletext', 'Content-Type':'application/json','Authorization':'key='+apikey})
+	headers = {'Referer': 'https://www.example.com/exampletext'}
+	response = requests.post(url, data=postdata, verify=False, headers={'Referer': 'https://www.example.com/exampletext', 'Content-Type':'application/json','Authorization':'key='+apikey})
 	if response.status_code == 200:
 		print("API key is \033[1;31;40m vulnerable \033[0mfor FCM API! Here is the PoC curl command which can be used from terminal:")
 		print("curl --header \"Authorization: key="+apikey+"\" --header Content-Type:\"application/json\" https://fcm.googleapis.com/fcm/send -d '{\"registration_ids\":[\"ABC\"]}'")
